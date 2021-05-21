de25bc526a5792937bb3fccc3e42fed4052b8626
Kevin Silvestre <kevsilvo@gmail.com>
2021-05-21 09:12:47 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <form>
+        <input id="nameInput" name="name" type="text" />
+      </form>
+    </div>
+  );
+}
