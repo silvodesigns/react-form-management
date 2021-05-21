6375d802ef43e6509b538c872195bca9a9b1f2c0
Kevin Silvestre <kevsilvo@gmail.com>
2021-05-21 09:30:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ export default function App() {
   return (
     <div className="App">
       <form>
-        <input id="nameInput" name="name" type="text" />
+        <label htmlFor="nameInput">Name</label>
+        <input maxLength="20" id="nameInput" name="name" type="text" />
+        <br />
+
+        <label htmlFor="God">God</label>
+        <select id="God">
+          <option value="1">Jesus</option>
+          <option value="2">Father</option>
+          <option value="3">Holy Spirit</option>
+        </select>
+        <br />
+
+        <label htmlFor="answer">Do you love him ?</label>
+        <input type="checkbox" id="answer" name="Do you love him?" />
+        <br />
+
+        <input type="submit" />
       </form>
     </div>
   );
